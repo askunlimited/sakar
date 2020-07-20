903017c2311456a3d6ddc2bd63f909b1d1c52d3a
efactor <efactoronline@gmail.com>
2020-07-20 11:52:38 +0100

    Adding the Model form for product uploads

---
+++ products/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.forms import ModelForm
+
+from .models import *
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
--- products/views.py
+++ products/views.py
@@ -1,6 +1,9 @@
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.shortcuts import render
+
+
 from .models import Product
+from .forms import *
 
 
 
@@ -8,3 +11,16 @@ def index(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'index.html', context)
+
+
+def uploads(request):
+    form = ProductForm()
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('uploads/')
+
+    context = {'form':form}
+    return render(request, 'uploads.html', context)
